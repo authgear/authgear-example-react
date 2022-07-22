76fae7dec902cbea21b3f4dc63bc3a6a699b9122
andychow326 <andychchow326@gmail.com>
2022-07-22 14:47:24 +0800

    Remove create-react-app and use Parcel

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import authgear from '@authgear/web';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authgear from "@authgear/web";
 
 authgear
   .configure({
-    endpoint: 'https://react-tutorial.authgearapps.com',
-    clientID: 'bdeabc526d0ddacf',
-    sessionType: 'refresh_token'
+    endpoint: "https://react-tutorial.authgearapps.com",
+    clientID: "bdeabc526d0ddacf",
+    sessionType: "refresh_token",
   })
   .then(
     () => {
-      console.log('Success');
+      console.log("Success");
     },
-    err => {
+    (err) => {
       console.log(err);
     }
   );
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("react-app-root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
