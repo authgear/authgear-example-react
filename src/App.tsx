d639185aa194517efbefa5d1137e8ae95a796782
andychow326 <andychchow326@gmail.com>
2022-07-22 15:37:24 +0800

    Remove redundent div element

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import AuthRedirect from "./AuthRedirect";
-import { UserContextProvider } from "./UserContext";
+import UserContextProvider from "./context/UserProvider";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <UserContextProvider>
       <Router>
-        <div>
-          <Routes>
-            <Route path="/auth-redirect" element={<AuthRedirect />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/auth-redirect" element={<AuthRedirect />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </UserContextProvider>
   );
-}
+};
 
 export default App;
