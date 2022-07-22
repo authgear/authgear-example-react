8f76cc31a43f4e916759dac3cf3d3bb0061d7e8b
andychow326 <andychchow326@gmail.com>
2022-07-22 15:45:17 +0800

    Remove redundent stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import AuthRedirect from "./AuthRedirect";
 import UserContextProvider from "./context/UserProvider";
-import "./App.css";
 
 const App: React.FC = () => {
   return (
