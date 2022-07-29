3b741c0bc48724e79665e291ba63eddb23236522
andychow326 <andychchow326@gmail.com>
2022-07-29 14:35:09 +0800

    Add line break after logout button

--- src/Home.tsx
+++ src/Home.tsx
@@ -70,6 +70,7 @@ const Home: React.FC = () => {
           <button type="button" onClick={logout}>
             Logout
           </button>
+          <br />
           <a target="_blank" rel="noreferrer" href={`${endpoint}/settings`}>
             User Setting
           </a>
